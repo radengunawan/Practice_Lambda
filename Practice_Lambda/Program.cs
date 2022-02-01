cafa50d5616a7419a503129205c01610965b5b4a
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-02-01 10:38:35 +0800

    Add project files.

---
+++ Practice_Lambda/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice_Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> angka = new List<int> { 1,2,3,4,5,9,8,7,6};
+
+        }
+    }
+}
