9650abce0c8236f6f3d065378d891217b8e44320
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-02-01 12:46:23 +0800

    another function

--- Practice_Lambda/Program.cs
+++ Practice_Lambda/Program.cs
@@ -7,7 +7,13 @@ namespace Practice_Lambda
     {
         static void Main(string[] args)
         {
-            List<int> angka = new List<int> { 1,2,3,4,5,9,8,7,6};
+            //List<int> angka = new List<int> { 1,2,3,4,5,9,8,7,6};
+
+            Func<int, int, int> kuadra = (x,y) => x + y*y;
+
+            Console.WriteLine(kuadra(11,12));
+
+            Console.ReadLine();
 
         }
     }
